6c55e1b82928c938c02260e92efb6d31ef066587
SaurabhCegian <saurabh.cegian@gmail.com>
2026-02-07 19:14:33 +0530

    First Agenet Developed

--- ReAct.py
+++ ReAct.py
@@ -3,14 +3,14 @@ from dotenv import load_dotenv
 from langchain_core.messages import BaseMessage, ToolMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 from langchain_core.tools import tool
-from langgraph.graph.message import add_message
+from langgraph.graph.message import add_messages
 from langgraph.graph import StateGraph, END
 from langgraph.prebuilt import ToolNode
 
 load_dotenv()
 
 class AgentState(TypedDict):
-    messages: Annotated[Sequence[BaseMessage], add_message]
+    messages: Annotated[Sequence[BaseMessage], add_messages]
 
 @tool
 def add(a: int, b: int) -> int:
@@ -19,10 +19,47 @@ def add(a: int, b: int) -> int:
 
 tools = [add]
 
-model = ChatOpenAI(model="gpt-4o", bind_tools=tools)
+model = ChatOpenAI(model="gpt-4o").bind_tools(tools)
 
 def model_call(state:AgentState):
     system_prompt = SystemMessage(content="You are my AI Assistant, Please answer my query to the best of your ability.")
     response = model.invoke([system_prompt] + state["messages"])
     return {"messages": [response]}
 
+def should_continue(state: AgentState):
+    messages = state["messages"]
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return "end"
+    else:
+        return "continue"
+
+graph = StateGraph(AgentState)
+graph.add_node("our_agent", model_call)
+
+tool_node = ToolNode(tools = tools)
+graph.add_node("tools", tool_node)
+
+graph.set_entry_point("our_agent")
+graph.add_conditional_edges(
+    "our_agent", 
+    should_continue,
+    {
+        "continue": "tools",
+        "end": END
+    }
+)
+
+graph.add_edge("tools", "our_agent")
+app = graph.compile()
+
+def print_stream(stream):
+    for s in stream:
+        message = s["messages"][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+inputs = {"messages": [("user", "Add 3+4.")]}
+print_stream(app.stream(inputs, stream_mode="values"))
